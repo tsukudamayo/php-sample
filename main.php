e2c9f863b7e8b7484082765ff89623ee1e70d344
tsukudamayo <tsukudamayo@gmail.com>
2025-02-20 14:01:14 +0900

    test2

--- main.php
+++ main.php
@@ -1,6 +1,17 @@
 <?php
+session_start();
+
 echo "Hello, World!\n";
 
 // 現在の日付と時刻を表示
 echo "Current date and time: " . date("Y-m-d H:i:s") . "\n";
+
+// 訪問回数をカウント
+if (!isset($_SESSION['visit_count'])) {
+    $_SESSION['visit_count'] = 1;
+} else {
+    $_SESSION['visit_count']++;
+}
+
+echo "You have visited this page " . $_SESSION['visit_count'] . " times.\n";
 ?>
